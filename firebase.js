bc906638b0d05a84b49006bccdcf5ba6437b4089
Leonel-David-Navarro-Meneses <112946720+Leonel-David-Navarro-Meneses@users.noreply.github.com>
2022-11-18 20:39:50 -0600

    commit 5

--- firebase.js
+++ firebase.js
@@ -1,24 +1,49 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getFirestore, collection , addDoc} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAQDhp66POu_SfAi4YBFoZisM3KsWsBptw",
-  authDomain: "fir-javascript-crud-cb557.firebaseapp.com",
-  projectId: "fir-javascript-crud-cb557",
-  storageBucket: "fir-javascript-crud-cb557.appspot.com",
-  messagingSenderId: "176328818606",
-  appId: "1:176328818606:web:7fa2fc932f92262c28923f"
+  // Put you credentials here
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
-const db = getFirestore()
+export const db = getFirestore();
 
-export const saveTask = (title, description) => {}
-    addDoc(collection(db, 'tasks'), {title, description});
+/**
+ * Save a New Task in Firestore
+ * @param {string} title the title of the Task
+ * @param {string} description the description of the Task
+ */
+export const saveTask = (title, description) =>
+  addDoc(collection(db, "tasks"), { title, description });
 
+export const onGetTasks = (callback) =>
+  onSnapshot(collection(db, "tasks"), callback);
+
+/**
+ *
+ * @param {string} id Task ID
+ */
+export const deleteTask = (id) => deleteDoc(doc(db, "tasks", id));
+
+export const getTask = (id) => getDoc(doc(db, "tasks", id));
+
+export const updateTask = (id, newFields) =>
+  updateDoc(doc(db, "tasks", id), newFields);
+
+export const getTasks = () => getDocs(collection(db, "tasks"));
